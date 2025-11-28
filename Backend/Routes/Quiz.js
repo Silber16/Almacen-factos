b5086526d22dcccaa42e8bee053624a4a99f6e2f
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-11-28 18:01:14 -0300

    se corrige archivo rutas del quiz.

--- Backend/Routes/Quiz.js
+++ Backend/Routes/Quiz.js
@@ -1,1 +1,14 @@
-const express 
+const express = require('express');
+const router = express.Router();
+const quizController = require('../Controllers/quizController');
+
+//este url se ejecuta cuando se muestra la pregunta al usuario
+router.get('/pregunta', quizController.obtenerPregunta);
+
+//este url se ejecuta cuando el usuario elige verdadero o falso
+router.post('/respuesta', quizController.validarRespuesta);
+
+
+module.exports = router;
+
+//en el controlador del quiz van las funciones que se realizan al ejecutarse estos urls
