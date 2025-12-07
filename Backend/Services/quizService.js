ca37d70f091c6f3d6d6dbff3881651dc7e508b76
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-06 23:29:26 -0300

    defino funcion obtenerPreguntaRandom, que le pide al repository que traiga un facto random de la bd. Si no encuentra o esta vacio devuelve null. (el controlador maneja el error.

--- Backend/Services/quizService.js
+++ Backend/Services/quizService.js
@@ -1,1 +1,11 @@
 const quizRepository = require('../Repositories/quizRepository');
+
+async function obtenerPreguntaRandom() {
+    try {
+        const facts = await quizRepository.getRandomFact();
+        
+        if (!facts || facts.length === 0) {
+            return null;
+        } 
+    }
+}
