23d3b97e8bced6f3dba40e78374f417466b138f4
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-06 23:58:09 -0300

    se agrega la logica de que facto mostrar, si el original o el modificado.

--- Backend/Services/quizService.js
+++ Backend/Services/quizService.js
@@ -1,11 +1,27 @@
 const quizRepository = require('../Repositories/quizRepository');
 
+
+//traer facto
 async function obtenerPreguntaRandom() {
     try {
         const facts = await quizRepository.getRandomFact();
         
         if (!facts || facts.length === 0) {
             return null;
-        } 
+        }
+        
+        const facto = facts[0];
+
+        //aca se elige aleatoriamente 50/50 si mostrar el facto verdadero o el falso
+        const mostrarModificado = Math.random() < 0.5;
+
+        //si el numero que salio es mayor a 0.5 se muestra el original
+        //si el numero que salio es menor a 0.5 se muestra el modificado
+        let textoMostrado;
+        if (mostrarModificado) {
+            textoMostrado = facto.contenido_modificado;
+        } else {
+            textoMostrado = facto.contenido
+        }
     }
 }
