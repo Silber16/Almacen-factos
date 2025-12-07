a40570684459b8cfa5caf7ba2656e5a35706ac91
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-07 17:50:26 -0300

    obtengo elementos del dom

--- Frontend/js/quiz.js
+++ Frontend/js/quiz.js
@@ -1,1 +1,19 @@
 //obtener elementos del DOM
+const lobby = document.getElementById('lobby');
+const playing = document.getElementById('playing');
+const result = document.getElementById('result');
+
+const btnJugar = document.getElementById('btn-jugar');
+const btnVerdadero = document.getElementById('btn-verdadero');
+const btnFalso = document.getElementById('btn-falso');
+const btnSiguiente = document.getElementById('btn-siguiente');
+const btnSalir = document.getElementById('btn-salir');
+
+const preguntaTexto = document.getElementById('pregunta-texto');
+const timerTexto = document.getElementById('timer-texto');
+const timerProgreso = document.getElementById('timer-progreso');
+
+const resultadoIcono = document.getElementById('resultado-icono');
+const resultadoMensaje = document.getElementById('resultado-mensaje');
+const puntosGanados = document.getElementById('puntos-ganados');
+const puntajeTotal = document.getElementById('puntaje-total');
