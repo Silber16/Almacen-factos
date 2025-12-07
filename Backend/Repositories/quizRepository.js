c38cbb39700d696fbc205a3006e16e235ceff648
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-06 21:11:44 -0300

    se corrige getRandomFact. Se cre la funcion getFactByID.

--- Backend/Repositories/quizRepository.js
+++ Backend/Repositories/quizRepository.js
@@ -10,5 +10,16 @@ async function getRandomFact() {
         LIMIT 1;
     `;
         const result = await db.query(query);
-        return result.rows[0];
+        return result.rows;
+}
+
+//recibe el id del facto que se le mostro al usuario para validar la respuesta del usuario
+async function getFactById(id) {
+    const query = `
+        SELECT content, quiz_content 
+        FROM facts
+        WHERE id = $1;
+    `;
+    const result = await db.query(query, [id]);
+    return result.rows
 }
