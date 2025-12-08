89e5bc9cd1782020fcac2c9cef0cadaaa5cb4a89
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-08 16:32:42 -0300

    agrego cors al appjs

--- Backend/App.js
+++ Backend/App.js
@@ -1,8 +1,13 @@
 const express = require ( 'express' ); 
+const cors = require('cors');
 const db = require ( './config/db' ); 
 const quizRoutes = require('./Routes/quizRoutes');
 
 const app = express (); 
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}));
 
 app.use(express.json());
 
