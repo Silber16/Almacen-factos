aff86f951b0900aafbd7badf341d4c5823f3d4db
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-09 12:45:11 -0300

    modifico index js

--- Backend/index.js
+++ Backend/index.js
@@ -6,4 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("SERVIDOR INICIADO");
+    console.log(`---------------------`);
+    console.log(`URL: http://localhost:${PORT}`);
+    console.log(`Entorno: ${process.env.NODE_ENV || 'development'}`);
 })
