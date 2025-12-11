5e316ce69ecba124c80d235b5d37259da8296f2b
Gaspar2005 <jchab@fi.uba.ar>
2025-12-11 17:05:16 -0300

    creo carpeta llamada middleware y creo archivo auth.js para implementar el token

---
+++ Backend/Middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+module.exports =function(req, res, next){
+    const authHeader = req.headers.auhtorization;
+    if(!authHeader){
+        return res.status(401).json({ error : "token requerido"});
+    }
+    const token = authHeader.split(" ")[1];
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch (err){
+        res.status(401).json({ error : "Token invalido"});
+    }
+}
