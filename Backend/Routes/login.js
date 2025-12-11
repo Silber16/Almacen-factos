4ad41a98e21a9a967cbf1c15cd2fa0cec36448d7
Gaspar2005 <jchab@fi.uba.ar>
2025-12-11 17:44:57 -0300

    agregar ruta protegida /me con middleware de autenticacion

--- Backend/Routes/login.js
+++ Backend/Routes/login.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const loginController = require('../Controllers/login');
+const auth = require(' ../middleware/auth');
 router.post('/register' , logincontroller.register);
 router.post('/login' , logincontroller.login);
+router.get(' /me' , loginController.me);
 module.exports = router;
