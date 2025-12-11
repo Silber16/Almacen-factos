8344a39d9c675b38ccf36e426014b6fdcf54d6da
Gaspar2005 <jchab@fi.uba.ar>
2025-12-11 03:19:31 -0300

    agregue nuevas columnas para el registro e inicio de sesion en users

--- Backend/Models/Users.js
+++ Backend/Models/Users.js
@@ -1,8 +1,13 @@
 class User {
   constructor(id, name, username) {
-    this.id = id;
-    this.name = name;
-    this.username = username;
+      this.id = id;
+      this.name = name;
+      this.last_name = last_name;
+      this.username = username;
+      this.email = email;
+      this.password = password;
+      this.birth_date = birth_date;
+      this.score = score;
   }
 }
 
