d8f6fb33ff3b51cf0c38cbf33a2e656aae881c63
Nacho Moreno Vivot <nahomorenov04@gmail.com>
2025-12-12 13:42:57 -0300

    se crea el repository del perfil, por ahora sin repo.

---
+++ Backend/Repositories/profileRepository.js
@@ -0,0 +1,60 @@
+const db = require('../config/db.js');
+
+//obtener usuario por id
+async function getUserById(userId) {
+    const query = `
+        SELECT id, name, username, score, bio, profile_picture, created_at
+        FROM users
+        WHERE id= $1;
+    `;
+    const result = await db.query(query, [userId]);
+    return result.rows[0];
+}
+
+
+//actualizar o editar perfil
+async function updateUserProfile(userId, bio, profilePicture) {
+    const query = `
+        UPDATE users
+        SET bio = $1, profile_picture = $2
+        WHERE id = $3
+        RETURNING id, naame, username, score, bio, profile_picture, created_at;
+    `;
+    const result = await db.query(query [bio, profilePicture, userId]);
+    return result.rows[0];
+}
+
+
+//obtener los factos publicados por el usuario, van a aparecer tipo twitter
+async function getUserFactos(userId) {
+    const query = `
+        SELECT id, content, font, created_at
+        FROM facts
+        WHERE createdby = $1
+        ORDER BY created_at DESC;
+    `;
+    const result = await db.query(query, [userId]);
+    return result.rows;
+}
+
+
+//obtener logros del usuario
+async function getUserTrophies(userId) {
+    const query = `
+        SELECT t.id, t.title, t.description, t.iconurl, t.pointsneeded, t.category, ut.earned_at
+        FROM trophies t
+        INNER JOIN user_trophy ut ON t.id = ut.trophyid
+        WHERE ut.usersid = $1
+        ORDER BY ut.earned_at DESC;
+    `;
+    const result = await db.query(query, [userId]);
+    return result.rows;
+}
+
+
+module.exports = {
+    getUserById,
+    updateUserProfile,
+    getUserFactos,
+    getUserTrophies
+}
