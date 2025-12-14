ed79b4f18496b5cb992ad8c91bd05fadd6653eed
Gaspar2005 <jchab@fi.uba.ar>
2025-12-13 21:16:13 -0300

    configuro el CORS para que funcione el POST GET PUT ETC EN EL LOGIN Y EL REGISTER

--- Backend/App.js
+++ Backend/App.js
@@ -1,19 +1,31 @@
-const express = require ( 'express' ); 
-const db = require ( './config/db' ); 
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
-const app = express (); 
+const db = require('./config/db');
+
+const app = express();
+
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
 app.get('/', async (req, res) => {
     try {
         const result = await db.query('SELECT * FROM facts');
-        console.log(result.rows);
         res.json(result.rows);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error interno del servidor');
     }
 });
-const authroutes = require('./Routes/login');
+
+const authroutes = require('./Routes/login.js');
 app.use('/api/auth', authroutes);
+
 module.exports = app;
