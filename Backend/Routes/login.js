67a04f28c6e5611840d25dd61b62b0703c19b2c3
Gaspar2005 <jchab@fi.uba.ar>
2025-12-13 21:13:19 -0300

    agrego rutas del login

--- Backend/Routes/login.js
+++ Backend/Routes/login.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const loginController = require('../Controllers/login');
-const auth = require(' ../middleware/auth');
+const logincontroller = require('../Controllers/login.js');
+const auth = require('../middleware/auth');
 router.post('/register' , logincontroller.register);
 router.post('/login' , logincontroller.login);
-router.get(' /me' , loginController.me);
+router.get('/me', auth, logincontroller.me);
 module.exports = router;
