8a7017752d96f3dc4175d895b64d435ece4ba04e
Gaspar2005 <jchab@fi.uba.ar>
2025-12-13 21:27:43 -0300

    arreglo error de tipeo

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log("SERVIDOR INICIADO");
-})
+app.listen(PORT,() => {
+    console.log("SERVIDOR INICIADO EN 0.0.0.0:" + PORT);
+});
