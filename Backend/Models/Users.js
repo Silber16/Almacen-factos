22606e87079c9616b551c5cba52bb0a81b28e7ef
Gaspar2005 <jchab@fi.uba.ar>
2025-12-14 17:47:08 -0300

    elimino lo que tenga que ver con el name , last_name, y birth_date en el archivo de models de la tabla users

--- Backend/Models/Users.js
+++ Backend/Models/Users.js
@@ -1,12 +1,9 @@
 class User {
-  constructor(id, name, username) {
+  constructor(id, username) {
       this.id = id;
-      this.name = name;
-      this.last_name = last_name;
       this.username = username;
       this.email = email;
       this.password = password;
-      this.birth_date = birth_date;
       this.score = score;
   }
 }
