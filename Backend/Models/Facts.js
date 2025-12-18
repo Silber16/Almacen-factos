ea2c7d55ae3ec1f0d1511943ca12314f245cd0c8
Francisco Silberberg <fransilber16@gmail.com>
2025-12-18 01:05:38 -0300

    Se modifica la entidad de Facts

--- Backend/Models/Facts.js
+++ Backend/Models/Facts.js
@@ -1,11 +1,14 @@
 class Facts {
-  constructor(Id, Title, Content, Font, Category, createdBy) {
+  constructor(Id, Title, Content, Font, Category, CreatedBy, UserName, Ia_response, Ia_verdict) {
     this.Id = Id;
     this.Title = Title;
     this.Content = Content;
     this.Font = Font;
+    this.CreatedBy = CreatedBy;
+    this.UserName = UserName;
     this.Category = Category;
-    this.CreatedBy = createdBy;
+    this.ia_response = Ia_response;
+    this.ia_verdict = Ia_verdict;
   }
 }
 
