a0b56fcc18927a6a88a23ccf6642a05061569076
Francisco Silberberg <fransilber16@gmail.com>
2025-12-18 01:05:18 -0300

    correccion de sintaxis commonJs - ES Modules

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,7 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
   user: process.env.DATABASE_USERNAME,
@@ -12,6 +14,4 @@ const pool = new Pool({
   }
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params)
-};
+export const query = (text, params) => pool.query(text, params);
