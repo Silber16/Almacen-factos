4304faa8998ae0b233719f081a5ec1a65a36cb4f
Gaspar2005 <jchab@fi.uba.ar>
2025-12-19 13:26:23 -0300

    llevo todas las rutas del ranking al app.js

--- backend/App.js
+++ backend/App.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import factsRoutes from './routes/Facts.js';
 import iaRoutes from './routes/Ia.js';
 import authRoutes from './routes/login.js';
+import RankingRoutes from './routes/ranking.js';
 
 import cors from 'cors';
 const app = express (); 
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/facts', factsRoutes);
 app.use('/api/ia', iaRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/ranking", RankingRoutes);
 // app.use('/api/trophies', trophyRoutes);
 // app.use('/api/users', usersRoutes);
 // app.use('/api/almacen-facts', almacenFactsRoutes);
