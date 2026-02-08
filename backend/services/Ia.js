e6499feab34dcaec0a9f74f9a56f745ff65deb99
Francisco Silberberg <fransilber16@gmail.com>
2026-02-08 17:15:02 -0300

    Se soluciona el problema con el prompt al crear un facto, se agrega logica para consumir el prompt desde un txt aparte, se modifican detalles en el html y css del boton para crear facts

--- backend/services/Ia.js
+++ backend/services/Ia.js
@@ -1,56 +1,18 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from 'dotenv';
+import path from 'path';
+import { readFile } from 'node:fs/promises';
 
-//se cargan las env
 dotenv.config();
 
-//se inicializa gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
 
 /*obitene la verifiacion y la pregunta del quiz en una sola llamada, para ahorrar tokens.
 se usa cuando el usuario CREA el facto.*/
 async function analyzeFact(title, content) {
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"  });
-        const prompt = `
-        Analiza el siguiente texto:
-        Título: "${title}"
-        Contenido: "${content}"
-
-        TU TAREA (Responde ÚNICAMENTE con un JSON válido):
-
-        1. VERIFICACIÓN:
-            Genera un veredicto claro y conciso (máximo 300 caracteres), argumentando si crees que la información es verdadera o falsa.
-            Expresalo como una opinión basada en argumentos solidos.
-
-        2. QUIZ:
-            Genera una afirmación o pregunta muy breve, clara y directa para un juego de Verdadero o Falso basada en este tema.
-            REGLAS DE ORO PARA EL QUIZ:
-                - CONTEXTO: Imagina que el jugador NO ha leído el texto original. La afirmación debe tener sentido por sí sola.
-                - PROHIBIDO: Nunca uses frases como "El texto sugiere", "Según el autor", "Como se menciona arriba" o "En este artículo".
-                - FORMATO: Debe ser una sentencia directa sobre el mundo real (histórica, científica, curiosa).
-    
-            LÓGICA VERDADERO/FALSO:
-                - Tira una moneda (50% probabilidad).
-                - Si decides que sea VERDADERA: Extrae el dato más curioso y afírmalo directamente.
-                - Si decides que sea FALSA: Toma un dato clave y cámbialo sutilmente para que suene plausible pero sea incorrecto.
-                - Define la dificultad (easy, medium, hard).
-
-        ESTRUCTURA JSON OBLIGATORIA:
-        {
-            "verification": {
-                "verdict": "Verdadero" | "Falso" | "Impreciso",
-                "explanation": "Aquí va el desarrollo de tus argumentos (lo que antes iba después del guion)"
-            },
-            "quiz": {
-                "question_text": "Afirmación corta para el juego",
-                "correct_answer": true, 
-                "explanation": "Explicación breve",
-                "difficulty": "medium"
-            }
-        }
-        `;
+        const prompt = await processPrompt(title, content);
         const result = await model.generateContent(prompt);
         const text = result.response.text().replace(/```json/g, '').replace(/```/g, '').trim();
 
@@ -67,6 +29,17 @@ async function analyzeFact(title, content) {
         quiz: null
         };
     }
+}
+
+async function processPrompt(title, content){
+    const promptPath = path.join(process.cwd(), 'resources', 'prompts', 'promptTemplate.txt');
+    const promptTemplate = await readFile(promptPath, 'utf-8');
+
+    const prompt = promptTemplate
+        .replace('{title}', title)
+        .replace('{content}', content);
+
+    return prompt;
 }
 
 export { analyzeFact }
