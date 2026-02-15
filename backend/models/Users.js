6cf175153ffc0cf8f2461c5d8b34a790cca16cd7
Francisco Silberberg <fransilber16@gmail.com>
2026-02-15 20:23:12 -0300

    Se modifica la clase User: Nombres en camelCase y uso de {} en el constructor para que desestructure el objeto que reciba.

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,11 +1,11 @@
 class User {
-  constructor(id, name ,username, bio, profile_picture, created_at, score) {
+  constructor({ id, name, username, bio, profilePicture, createdAt, score }) {
     this.id = id;
     this.name = name;
     this.username = username;
     this.bio = bio;
-    this.profile_picture = profile_picture;
-    this.created_at = created_at;
+    this.profilePicture = profilePicture;
+    this.createdAt = createdAt; 
     this.score = score;
   }
 }
