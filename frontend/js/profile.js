ccf7bc531894e1435fc8f61acdb76e069edce5c9
Gaspar2005 <jchab@fi.uba.ar>
2026-02-20 19:07:01 -0300

    arreglamos bug de la carga de la foto de perfil

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -387,11 +387,9 @@ async function handleEditProfile(e) {
     formData.append('name', nameVal);
     formData.append('username', userVal);
     formData.append('bio', bioVal);
-
+    formData.append("deletePicture", deleteFlag ? "true" : "false");
     if (picInput.files && picInput.files[0]) {
         formData.append('profilePicture', picInput.files[0]);
-    } else if (deleteFlag) {
-        formData.append('profilePicture', '');
     }
 
     try {
